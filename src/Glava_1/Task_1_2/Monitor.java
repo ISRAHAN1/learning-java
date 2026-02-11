796fe269b3222fb07476e26e8dbfc7b6042c9a61
User <muhamedzoda_18@mail.ru>
2026-02-12 01:40:41 +0300

    new work

---
+++ src/Glava_1/Task_1_2/Monitor.java
@@ -0,0 +1,15 @@
+package Glava_1.Task_1_2;
+
+public class Monitor {
+    public static void main(String[] args) {
+        int monitors = 50;
+        int programmers = 2;
+        System.out.println(Monitor.drawMonitors(monitors, programmers));
+
+    }
+
+    public static int drawMonitors(int a, int b) {
+        return a % b;
+
+    }
+}
